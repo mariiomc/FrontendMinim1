dc2d34272c37a9dd26e3a3e780dc43315766a5a2
intxz <angel.redondo@estudiantat.upc.edu>
2024-04-15 23:41:43 +0200

    visual hosing not finished, places finished

---
+++ src/app/models/housing.ts
@@ -0,0 +1,31 @@
+import * as mongoose from 'mongoose';
+
+export interface IHousing {
+    _id?: string; // Optional _id field
+    title: string;
+    description: string;
+    owner: string; // Reference to the User collection
+    reviews?: string[];
+    rating: string;
+    coords: {
+        latitude: string;
+        longitude: string;
+    };
+    photo: string;
+    address: string;
+    availability: boolean;
+    coffee: boolean; //true only "coffee" 
+    schedule: {
+        monday: string;
+        tuesday: string;
+        wednesday: string;
+        thursday: string;
+        friday: string;
+        saturday: string;
+        sunday: string;
+    };
+    verified: boolean;
+    house_deactivated:boolean;
+    creation_date?: Date;
+    modified_date?: Date;
+}
