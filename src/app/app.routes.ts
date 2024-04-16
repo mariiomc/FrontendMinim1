763e789d01251a65eff9962c1f2e87a66d142812
mariiomc <101208018+mariiomc@users.noreply.github.com>
2024-04-16 10:41:21 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,13 @@ import { Routes } from '@angular/router';
 import { UserComponent } from './components/user/user.component';
 import { PlaceComponent } from './components/place/place.component';
 import { ReviewComponent } from './components/review/review.component';
-import { HousingComponent } from './components/housing/housing.component';
+//import { HousingComponent } from './components/housing/housing.component';
 import { ConversationComponent } from './components/conversation/conversation.component';
 
 export const routes: Routes = [
     {path: 'user',title:'User', component: UserComponent}, 
     {path: 'place',title:'Place', component: PlaceComponent},
     {path: 'review',title:'Review', component: ReviewComponent},
-    {path: 'housing',title:'Housing', component: HousingComponent},
+    //{path: 'housing',title:'Housing', component: HousingComponent},
     {path: 'conversation',title:'Conversation', component: ConversationComponent}
     ];
