0b95b038fc1092a55b2fe4437152637c1dde49f9
intxz <angel.redondo@estudiantat.upc.edu>
2024-04-16 10:34:24 +0200

    housing x

--- src/app/components/housing/housing.component.ts
+++ src/app/components/housing/housing.component.ts
@@ -1,209 +1,209 @@
-import { Component } from '@angular/core';
-import { IHousing } from '../../models/housing';
-import {FormsModule} from '@angular/forms';
-import { FormBuilder,FormControl, FormGroup, Validators } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
-import { Title } from '@angular/platform-browser';
-import { HousingService } from '../../services/housing.service';
-import mongoose from 'mongoose';
+// import { Component } from '@angular/core';
+// import { IHousing } from '../../models/housing';
+// import {FormsModule} from '@angular/forms';
+// import { FormBuilder,FormControl, FormGroup, Validators } from '@angular/forms';
+// import { ReactiveFormsModule } from '@angular/forms';
+// import { Title } from '@angular/platform-browser';
+// import { HousingService } from '../../services/housing.service';
+// import mongoose from 'mongoose';
 
 
-@Component({
-  selector: 'app-housing',
-  standalone: true,
-  imports: [FormsModule,ReactiveFormsModule],
-  templateUrl: './housing.component.html',
-  styleUrl: './housing.component.css'
-})
-export class HousingComponent {
+// @Component({
+//   selector: 'app-housing',
+//   standalone: true,
+//   imports: [FormsModule,ReactiveFormsModule],
+//   templateUrl: './housing.component.html',
+//   styleUrl: './housing.component.css'
+// })
+// export class HousingComponent {
 
-searchBarHouseString: string='';
-houses: IHousing[] = [];//users retrieved from the server
-selectedHouse: IHousing | null = null;
-houseSelected: boolean = false;
-createMode:boolean=false;
-searchHouseMode:boolean=false;
-editMode:boolean=false;
-searchedHouse: IHousing | null = null;
-deactivateHouseId:string='';
-houseToBeEdited: IHousing | null = null;
+// searchBarHouseString: string='';
+// houses: IHousing[] = [];//users retrieved from the server
+// selectedHouse: IHousing | null = null;
+// houseSelected: boolean = false;
+// createMode:boolean=false;
+// searchHouseMode:boolean=false;
+// editMode:boolean=false;
+// searchedHouse: IHousing | null = null;
+// deactivateHouseId:string='';
+// houseToBeEdited: IHousing | null = null;
 
-  houseForm = new FormGroup ({
-    title: new FormControl('', Validators.required),
-    description: new FormControl('', Validators.required),
-    owner: new FormControl('', Validators.required),
-    reviews: new FormControl([]),
-    rating: new FormControl('', Validators.required),
-    coord: new FormGroup({
-      latitude: new FormControl('', Validators.required),
-      longitude: new FormControl('', Validators.required)
-    }),
-    photo: new FormControl('', Validators.required),
-    address: new FormControl('', Validators.required),
-    availability: new FormControl(false, Validators.required),
-    coffee: new FormControl(false, Validators.required),
-    schedule: new FormGroup({
-      monday: new FormControl('', Validators.required),
-      tuesday: new FormControl('', Validators.required),
-      wednesday: new FormControl('', Validators.required),
-      thursday: new FormControl('', Validators.required),
-      friday: new FormControl('', Validators.required),
-      saturday: new FormControl('', Validators.required),
-      sunday: new FormControl('', Validators.required)
-    }),
-    verified: new FormControl(false, Validators.required),
-    house_deactivated: new FormControl(false, Validators.required),
-  });
+//   houseForm = new FormGroup ({
+//     title: new FormControl('', Validators.required),
+//     description: new FormControl('', Validators.required),
+//     owner: new FormControl('', Validators.required),
+//     reviews: new FormControl([]),
+//     rating: new FormControl('', Validators.required),
+//     coord: new FormGroup({
+//       latitude: new FormControl('', Validators.required),
+//       longitude: new FormControl('', Validators.required)
+//     }),
+//     photo: new FormControl('', Validators.required),
+//     address: new FormControl('', Validators.required),
+//     availability: new FormControl(false, Validators.required),
+//     coffee: new FormControl(false, Validators.required),
+//     schedule: new FormGroup({
+//       monday: new FormControl('', Validators.required),
+//       tuesday: new FormControl('', Validators.required),
+//       wednesday: new FormControl('', Validators.required),
+//       thursday: new FormControl('', Validators.required),
+//       friday: new FormControl('', Validators.required),
+//       saturday: new FormControl('', Validators.required),
+//       sunday: new FormControl('', Validators.required)
+//     }),
+//     verified: new FormControl(false, Validators.required),
+//     house_deactivated: new FormControl(false, Validators.required),
+//   });
 
-  constructor( public housingService: HousingService, private formBuilder: FormBuilder){}
+//   constructor( public housingService: HousingService, private formBuilder: FormBuilder){}
 
-  ngOnInit(): void {
-    // Fetch data from API
-    console.log('fetching houses');
-    this.housingService.getHouses().subscribe(houses => {
-      this.houses = houses;
-      console.log(this.houses);
-    })
-  }
+//   ngOnInit(): void {
+//     // Fetch data from API
+//     console.log('fetching houses');
+//     this.housingService.getHouses().subscribe(houses => {
+//       this.houses = houses;
+//       console.log(this.houses);
+//     })
+//   }
 
-  onSelectHouse(house: IHousing): void{
-    this.houseSelected = true;
-    this.selectedHouse = house;
-  }
+//   onSelectHouse(house: IHousing): void{
+//     this.houseSelected = true;
+//     this.selectedHouse = house;
+//   }
 
-  backToHouseList(): void{
-    this.houseSelected = false;
-    this.selectedHouse = null;
-    this.createMode = false;
-    this.searchHouseMode = false;
-    this.searchedHouse = null;
-    this.editMode = false;
-  }
+//   backToHouseList(): void{
+//     this.houseSelected = false;
+//     this.selectedHouse = null;
+//     this.createMode = false;
+//     this.searchHouseMode = false;
+//     this.searchedHouse = null;
+//     this.editMode = false;
+//   }
 
-  createHouseBtn(): void{
-    this.createMode = true;
-  }
+//   createHouseBtn(): void{
+//     this.createMode = true;
+//   }
 
-  refreshHouseList(): void {
-    // Fetch the updated user list from the server
-    this.housingService.getHouses().subscribe(houses => {
-      this.houses = houses;
-      console.log('House list updated:', this.houses);
-    });
-  }
+//   refreshHouseList(): void {
+//     // Fetch the updated user list from the server
+//     this.housingService.getHouses().subscribe(houses => {
+//       this.houses = houses;
+//       console.log('House list updated:', this.houses);
+//     });
+//   }
 
-  onSubmit(): void {
-    if (this.houseForm.valid) {
-      // Extract form values
-      const formValues = this.houseForm.value;
+//   onSubmit(): void {
+//     if (this.houseForm.valid) {
+//       // Extract form values
+//       const formValues = this.houseForm.value;
   
-      // Create a new house object from form values
-      const newHouse: IHousing = {
-        title: formValues.title || '',
-        description: formValues.description || '',
-        owner: formValues.owner || '',
-        reviews: formValues.reviews || [''],
-        rating: formValues.rating || '',
-        coords: {
-          longitude: (formValues.coord?.longitude || ''),
-          latitude: (formValues.coord?.latitude || ''),
-        },
-        photo: formValues.photo || '',
-        address: formValues.address || '',
-        availability: formValues.availability || false,
-        coffee: formValues.coffee || false,
-        schedule: {
-          monday: (formValues.schedule?.monday || ''),
-          tuesday: (formValues.schedule?.monday || ''),
-          wednesday: (formValues.schedule?.monday || ''),
-          thursday: (formValues.schedule?.monday || ''),
-          friday:(formValues.schedule?.monday || ''),
-          saturday: (formValues.schedule?.monday || ''),
-          sunday: (formValues.schedule?.monday || ''),
-        },
-        verified: formValues.verified || false,
-        house_deactivated: formValues.house_deactivated || false,
-      };
+//       // Create a new house object from form values
+//       const newHouse: IHousing = {
+//         title: formValues.title || '',
+//         description: formValues.description || '',
+//         owner: formValues.owner || '',
+//         reviews: formValues.reviews || [''],
+//         rating: formValues.rating || '',
+//         coords: {
+//           longitude: (formValues.coord?.longitude || ''),
+//           latitude: (formValues.coord?.latitude || ''),
+//         },
+//         photo: formValues.photo || '',
+//         address: formValues.address || '',
+//         availability: formValues.availability || false,
+//         coffee: formValues.coffee || false,
+//         schedule: {
+//           monday: (formValues.schedule?.monday || ''),
+//           tuesday: (formValues.schedule?.monday || ''),
+//           wednesday: (formValues.schedule?.monday || ''),
+//           thursday: (formValues.schedule?.monday || ''),
+//           friday:(formValues.schedule?.monday || ''),
+//           saturday: (formValues.schedule?.monday || ''),
+//           sunday: (formValues.schedule?.monday || ''),
+//         },
+//         verified: formValues.verified || false,
+//         house_deactivated: formValues.house_deactivated || false,
+//       };
   
-      this.housingService.createHouse(newHouse).subscribe({
-        next: (createdUser: IHousing) => {
-          console.log('User created:', createdUser);
-          // Optionally, reset the form after successful submission
-          this.refreshHouseList();
-          this.houseForm.reset();
-          this.createMode = false;
-          // You may also want to navigate the user back to the user list view or perform any other action
-        },
-        error: (error: any) => {
-          console.error('Error creating house:', error);
-          // Handle error cases
-        }
-      });
-    }
-  }
+//       this.housingService.createHouse(newHouse).subscribe({
+//         next: (createdUser: IHousing) => {
+//           console.log('User created:', createdUser);
+//           // Optionally, reset the form after successful submission
+//           this.refreshHouseList();
+//           this.houseForm.reset();
+//           this.createMode = false;
+//           // You may also want to navigate the user back to the user list view or perform any other action
+//         },
+//         error: (error: any) => {
+//           console.error('Error creating house:', error);
+//           // Handle error cases
+//         }
+//       });
+//     }
+//   }
 
-  searchForHouse(): void {
-    this.searchHouseMode = true;
-    if(this.searchBarHouseString != ''){
-      this.housingService.getHouse(this.searchBarHouseString).subscribe(house => {
-        this.searchedHouse = house;
-      });
-    }else{
-      this.searchedHouse = null;
-    }
-  }
+//   searchForHouse(): void {
+//     this.searchHouseMode = true;
+//     if(this.searchBarHouseString != ''){
+//       this.housingService.getHouse(this.searchBarHouseString).subscribe(house => {
+//         this.searchedHouse = house;
+//       });
+//     }else{
+//       this.searchedHouse = null;
+//     }
+//   }
 
-  deactivateHouse(): void {
-    if(this.selectedHouse){
-      this.deactivateHouseId = this.selectedHouse._id || '';
-    }else if(this.searchedHouse){
-      this.deactivateHouseId = this.searchedHouse._id || '';
-    }
-    this.housingService.deleteHouse(this.deactivateHouseId).subscribe(() => { // Removed empty parentheses
-      this.refreshHouseList();
-      this.backToHouseList();
-    });
-  }
+//   deactivateHouse(): void {
+//     if(this.selectedHouse){
+//       this.deactivateHouseId = this.selectedHouse._id || '';
+//     }else if(this.searchedHouse){
+//       this.deactivateHouseId = this.searchedHouse._id || '';
+//     }
+//     this.housingService.deleteHouse(this.deactivateHouseId).subscribe(() => { // Removed empty parentheses
+//       this.refreshHouseList();
+//       this.backToHouseList();
+//     });
+//   }
 
-  // editHouseMode(): void {
-  //   this.editMode = true;
-  //   if(this.selectedHouse){
-  //     this.houseToBeEdited = this.selectedHouse;
-  //   }else if(this.searchedHouse){
-  //     this.houseToBeEdited = this.searchedHouse;
-  //   }
-  //   this.houseForm.patchValue({
-  //     title: this.houseToBeEdited?.title || '',
-  //       description: this.houseToBeEdited?.description || '',
-  //       owner: this.houseToBeEdited?.owner || '',
-  //       reviews: this.houseToBeEdited?.reviews || [''],
-  //       rating: this.houseToBeEdited?.rating || '',
-  //       coords: {
-  //         longitude: (this.houseToBeEdited?.coords?.longitude || ''),
-  //         latitude: (this.houseToBeEdited?.coords?.latitude || ''),
-  //       },
-  //       photo: this.houseToBeEdited?.photo || '',
-  //       address: this.houseToBeEdited?.address || '',
-  //       availability: this.houseToBeEdited?.availability || false,
-  //       coffee: this.houseToBeEdited?.coffee || false,
-  //       schedule: {
-  //         monday: (this.houseToBeEdited?.schedule?.monday || ''),
-  //         tuesday: (this.houseToBeEdited?.schedule?.monday || ''),
-  //         wednesday: (this.houseToBeEdited?.schedule?.monday || ''),
-  //         thursday: (this.houseToBeEdited?.schedule?.monday || ''),
-  //         friday:(this.houseToBeEdited?.schedule?.monday || ''),
-  //         saturday: (this.houseToBeEdited?.schedule?.monday || ''),
-  //         sunday: (this.houseToBeEdited?.schedule?.monday || ''),
-  //       },
-  //       verified: this.houseToBeEdited?.verified || false,
-  //       house_deactivated: this.houseToBeEdited?.house_deactivated || false,
-  //   });
-  // }
+//   // editHouseMode(): void {
+//   //   this.editMode = true;
+//   //   if(this.selectedHouse){
+//   //     this.houseToBeEdited = this.selectedHouse;
+//   //   }else if(this.searchedHouse){
+//   //     this.houseToBeEdited = this.searchedHouse;
+//   //   }
+//   //   this.houseForm.patchValue({
+//   //     title: this.houseToBeEdited?.title || '',
+//   //       description: this.houseToBeEdited?.description || '',
+//   //       owner: this.houseToBeEdited?.owner || '',
+//   //       reviews: this.houseToBeEdited?.reviews || [''],
+//   //       rating: this.houseToBeEdited?.rating || '',
+//   //       coords: {
+//   //         longitude: (this.houseToBeEdited?.coords?.longitude || ''),
+//   //         latitude: (this.houseToBeEdited?.coords?.latitude || ''),
+//   //       },
+//   //       photo: this.houseToBeEdited?.photo || '',
+//   //       address: this.houseToBeEdited?.address || '',
+//   //       availability: this.houseToBeEdited?.availability || false,
+//   //       coffee: this.houseToBeEdited?.coffee || false,
+//   //       schedule: {
+//   //         monday: (this.houseToBeEdited?.schedule?.monday || ''),
+//   //         tuesday: (this.houseToBeEdited?.schedule?.monday || ''),
+//   //         wednesday: (this.houseToBeEdited?.schedule?.monday || ''),
+//   //         thursday: (this.houseToBeEdited?.schedule?.monday || ''),
+//   //         friday:(this.houseToBeEdited?.schedule?.monday || ''),
+//   //         saturday: (this.houseToBeEdited?.schedule?.monday || ''),
+//   //         sunday: (this.houseToBeEdited?.schedule?.monday || ''),
+//   //       },
+//   //       verified: this.houseToBeEdited?.verified || false,
+//   //       house_deactivated: this.houseToBeEdited?.house_deactivated || false,
+//   //   });
+//   // }
 
   
 
 
-}
+// }
 
 
 
--- src/app/services/housing.service.ts
+++ src/app/services/housing.service.ts
@@ -1,46 +1,46 @@
-import { Injectable } from '@angular/core';
-import { IHousing } from '../models/housing';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { AuthService } from './auth.service';
+// import { Injectable } from '@angular/core';
+// import { IHousing } from '../models/housing';
+// import { HttpClient, HttpHeaders } from '@angular/common/http';
+// import { AuthService } from './auth.service';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class HousingService {
-  token: string | null = null;
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class HousingService {
+//   token: string | null = null;
 
-  constructor(private http:HttpClient, private authService:AuthService) { }
+//   constructor(private http:HttpClient, private authService:AuthService) { }
 
-  url: string = "http://127.0.0.1:3000";
+//   url: string = "http://127.0.0.1:3000";
 
-  getToken() {
-    this.token = this.authService.getToken();
-  }
+//   getToken() {
+//     this.token = this.authService.getToken();
+//   }
 
-  getHeaders() {
-    this.getToken();
-    let headers = new HttpHeaders();
-    headers = headers.set('x-access-token', this.token || '');
-    return headers;
-  }
+//   getHeaders() {
+//     this.getToken();
+//     let headers = new HttpHeaders();
+//     headers = headers.set('x-access-token', this.token || '');
+//     return headers;
+//   }
 
-  createHouse(newHouse : IHousing|undefined) {
-    return this.http.post<IHousing>(this.url+'/housing',newHouse);
-  }
+//   createHouse(newHouse : IHousing|undefined) {
+//     return this.http.post<IHousing>(this.url+'/housing',newHouse);
+//   }
 
-  getHouse(findHouse : string){
-    return this.http.get<IHousing>(this.url+'/housingadmin/'+findHouse, { headers: this.getHeaders() });
-  }
+//   getHouse(findHouse : string){
+//     return this.http.get<IHousing>(this.url+'/housingadmin/'+findHouse, { headers: this.getHeaders() });
+//   }
 
-  getHouses() {
-    return this.http.get<IHousing[]>(this.url+'/housingadmin', { headers: this.getHeaders() });
-  }
+//   getHouses() {
+//     return this.http.get<IHousing[]>(this.url+'/housingadmin', { headers: this.getHeaders() });
+//   }
 
-  updateHouse(editHouse : IHousing) {
-    return this.http.put<IHousing>(this.url+'/housing/'+ editHouse._id, editHouse, { headers: this.getHeaders() });
-  }
+//   updateHouse(editHouse : IHousing) {
+//     return this.http.put<IHousing>(this.url+'/housing/'+ editHouse._id, editHouse, { headers: this.getHeaders() });
+//   }
 
-  deleteHouse(deleteHouseId : string) {
-    return this.http.delete(this.url+'/housing/'+ deleteHouseId, { headers: this.getHeaders() });
-  }
-}
+//   deleteHouse(deleteHouseId : string) {
+//     return this.http.delete(this.url+'/housing/'+ deleteHouseId, { headers: this.getHeaders() });
+//   }
+// }
